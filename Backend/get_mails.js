e89b2121430a5353dae6c109e415b4ecc8e5a66a
unknown <mustafaburakgurbuz@gmail.com>
2019-10-06 18:47:33 +0300

    email send-read functions implemented

---
+++ Backend/get_mails.js
@@ -0,0 +1,97 @@
+// Step 1: Include required modules
+var Imap = require('imap'),
+    inspect = require('util').inspect; 
+    var fs = require('fs'), fileStream; 
+
+
+function fetch_emails(imap, callback) {
+    var emails = []
+    function openInbox(cb) {
+        imap.openBox('INBOX', true, cb);
+    }
+      
+    imap.once('ready', function() {
+    openInbox(function(err, box) {
+    if (err) throw err;
+
+    // Change the date with the one from which you want receive emails
+    //Unseen means you'll only get mails that are unseen
+    imap.search(['ALL'], function(err, results) { 
+        if (err) throw err;
+
+        var f = imap.fetch(results, { bodies: '' });
+        f.on('message', function(msg, seqno) {
+        console.log('Message #%d', seqno); 
+        var prefix = '(#' + seqno + ') ';
+
+        msg.on('body', function(stream, info) {
+            console.log(prefix + 'Body');
+            const chunks = [];
+            stream.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+
+            // Send the buffer or you can put it into a var
+            stream.on("end", function () {
+                var mail = Buffer.concat(chunks).toString()
+                mail = mail.substring(mail.indexOf('From: '))
+                const from = mail.substring(mail.indexOf('<') + 1, mail.indexOf('>'));
+                const subject = mail.substring(mail.indexOf('Subject:') + 9, mail.indexOf('To:') - 2);
+                const content = mail.substring(mail.indexOf('text/plain') + 31 , mail.indexOf('text/html') - 50);
+                emails.push({
+                    from: from,
+                    subject: subject,
+                    content: content
+                })
+            });
+        });
+
+        msg.once('attributes', function(attrs) {
+            console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+        });
+        msg.once('end', function() {
+            console.log(prefix + 'Finished');
+        });
+        });
+        f.once('error', function(err) {
+        console.log('Fetch error: ' + err);
+        });
+        f.once('end', function() {
+        console.log('Done fetching all messages!');
+        imap.end();
+        });
+    });
+    });
+    });
+    
+    imap.once('error', function(err) {
+    console.log(err);
+    });
+    
+    imap.once('end', function() {
+        console.log('Connection ended');
+        callback(emails)
+    });
+    
+    imap.connect(); 
+}
+
+var imap = new Imap({
+    user: 'mail.system.test123@gmail.com', // example: aakanksha.jain8@gmail.com
+    password: 'mail_test', // Remember, using just password for authentication will only work if you have less secured apps enabled 
+    host: 'imap.gmail.com', 
+    port: 993,
+    tlsOptions: {
+      rejectUnauthorized: false
+      },
+    tls: true
+  });
+
+  
+fetch_emails(imap, function(emails) {
+    for(var i = 0; i < emails.length; i++ ){
+        console.log(emails[i])
+    }
+})
+
+
--- Frontend/src/email.js
+++ Frontend/src/email.js
@@ -3,7 +3,10 @@ import React from 'react';
 class Email extends React.Component {
   render(){
       return (
-        <h1>You are in!</h1>
+        <div className="text-center">
+          <h1>You are in!</h1>
+        </div>
+        
       )
   }
 }
