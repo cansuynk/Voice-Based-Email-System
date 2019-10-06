6e45015e220a858469106f1ceabf341f1ab352c8
unknown <mustafaburakgurbuz@gmail.com>
2019-10-06 16:00:38 +0300

    init2

---
+++ Frontend/src/email.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Email extends React.Component {
+  render(){
+      return (
+        <h1>You are in!</h1>
+      )
+  }
+}
+
+export default Email;
---
+++ Frontend/src/welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+  render(){
+      return (
+        <h1>Welcome Login or Signin</h1>
+      )
+  }
+}
+
+export default Welcome;
