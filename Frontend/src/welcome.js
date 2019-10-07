21fb064396a1c8fb50d5aa7fbf8ca2166ecb4759
cansuynk <cansuyanik@hotmail.com>
2019-10-07 18:30:50 +0300

    Updated welcome.js File

--- Frontend/src/welcome.js
+++ Frontend/src/welcome.js
@@ -8,109 +8,123 @@ class Welcome extends React.Component {
         this.state = {
             email: "",
             password: "",
+            email_for_registration: "",
+            password_for_registration: "",
           
         }
      
-
+        this.handleChange = this.handleChange.bind(this);
+        this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+        this.handleSignSubmit = this.handleSignSubmit.bind(this);
     }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleLoginSubmit(e) {
+        alert(this.state.email) //for testing
+        e.preventDefault()
+    }
+
+    handleSignSubmit(e) {
+        alert(this.state.email_for_registration) //for testing
+        e.preventDefault()
+    }
+
     render() {
 
         return (
-           
+                      
             <div className="page">
                 <div className="logo"></div>
                 <div className="header">
                     <h2>Welcome To The Voice Based Email System</h2>
-            </div>
+                </div>
                
-          <div className="content">
-              <div className="col-sm-8 main-section">
-               
-          
+                <div className="content">
+                    <div className="col-sm-8 main-section">
 
-              
-              Email
-                  <div className="form-group">     
-                      <input
-                          className="form-input"
-                          type="email" placeholder="Email"
-                          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,14}$"
-                          name="email"
-                          onInput={this.handleChange}
-                          value={this.state.email}
-                          required
-                      />
-              </div>
-                 Password 
-                  <div className="form-group"> 
-                      <input
-                          className="form-input"
-                          type="password"
-                          placeholder="Password"
-                          pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"
-                          onInput={this.handleChange}
-                          value={this.state.password}
-                          name="password"
-                          required
-                      />
+                        <form onSubmit={this.handleLoginSubmit}>
+                            Email
+                            <div className="form-group">     
+                                <input
+                                    className="form-input"
+                                    type="email" placeholder="Email"
+                                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,14}$"
+                                    name="email"
+                                    onInput={this.handleChange}
+                                    value={this.state.email}
+                                    required
+                                />
+                            </div>
+                            Password 
+                            <div className="form-group"> 
+                                <input
+                                    className="form-input"
+                                    type="password"
+                                    placeholder="Password"
+                                    onInput={this.handleChange}
+                                    value={this.state.password}
+                                    name="password"
+                                    required
+                                />
+                            </div>
+                            <br />
+
+                            <div className="form-group">
+                                <div className="btn-group btn-group-block">
+                                    <button className="btn btn-primary btn-block" type="submit" value="Submit">Login</button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <br />
+                        <div className="divider text-center" data-content="OR SIGN UP"></div>
+                        <form onSubmit={this.handleSignSubmit}>
+                            Email
+                            <div className="form-group">    
+                                <input
+                                    className="form-input"
+                                    type="email"
+                                    placeholder="Email"
+                                    onInput={this.handleChange}
+                                    value={this.state.registrationmail}
+                                    name="email_for_registration"
+                                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,14}$"
+                                    required
+                                />
+                            </div>
+
+                            Password 
+                            <div className="form-group">    
+                                <input
+                                    className="form-input"
+                                    type="password"
+                                    placeholder="Password"
+                                    onInput={this.handleChange}
+                                    value={this.state.registrationpassword}
+                                    name="password_for_registration"
+                                    required
+                                />
+                            </div>
+
+                             <br />
+                            <div className="form-group">
+                                <div className="btn-group btn-group-block">
+                                     <button className="btn btn-primary btn-block" type = "submit">Sign Up</button>
+                                </div>
+                            </div>
+                         </form>
                     </div>
-                      <br />
 
-                  <div className="form-group">
-                  <div className="btn-group btn-group-block">
-                  <button className="btn btn-primary btn-block" type="submit">Login</button>
-                      </div>
-                      </div>
+                </div>
 
-
-                      <br />
-
-
-                  <div className="divider text-center" data-content="OR SIGN UP"></div>
-
-                  
-              Email
-              <div className="form-group">    
-                      <input
-                          className="form-input"
-                          type="email"
-                          placeholder="Email"
-                          onInput={this.handleChange}
-                          value={this.state.email}
-                          name="email"
-                          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,14}$"
-                          required
-
-                      />
-              </div>
-              Password 
-                <div className="form-group">    
-              <input
-                      className="form-input"
-                      type="password"
-                      placeholder="Password"
-                      onInput={this.handleChange}
-                      value={this.state.password}
-                      name="password"
-                      pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"
-                      required
-
-                      />
-                      </div>
-                  <br />
-                  <div className="form-group">
-                  <div className="btn-group btn-group-block">
-                  <button className="btn btn-primary btn-block" type = "submit">Sign Up</button>
-                      </div>
-                  </div>
-               
-              </div>
-
-          </div>
-
-          </div>
-      )
-  }
+            </div>
+        )
+    }
 }
 
 export default Welcome;
