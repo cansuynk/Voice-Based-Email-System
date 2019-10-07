17cdd0938878ac41d7eb03a3435443d697bda01f
cansuynk <cansuyanik@hotmail.com>
2019-10-08 02:25:48 +0300

    Updated email page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,17 +6,62 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = { 
-      auth: false
+      auth: true
     }
   }
-  render(){
+    render() {
+
+        //This is the example structure of the mails. This example is used for testing the interface
+        var fixtures = [
+            {
+                id: 1,
+                name: "Inbox",
+                emails: [
+                    {
+                        id: 1,
+                        from: "joe@tryolabs.com",
+                        to: "fernando@tryolabs.com",
+                        subject: "Meeting",
+                        body: "hi"
+                    },
+                    {
+                        id: 2,
+                        from: "newsbot@tryolabs.com",
+                        to: "fernando@tryolabs.com",
+                        subject: "News Digest",
+                        body: "<h1>Intro to React</h1> <img src='https://raw.githubusercontent.com/wiki/facebook/react/react-logo-1000-transparent.png' width=300/)>"
+                    },
+                    {
+                        id: 2,
+                        from: "newsbot@tryolabs.com",
+                        to: "fernando@tryolabs.com",
+                        subject: "News Digest",
+                        body: "adaldmvladmvkadmvkladmvkdmvlkdmvladmvjsrýsrjfjý"
+                    }
+                ]
+            },
+            {
+                id: 2,
+                name: "Sent",
+                emails: [
+                    {
+                        id: 3,
+                        from: "nigerian.prince@gmail.com",
+                        to: "fernando@tryolabs.com",
+                        subject: "Obivous 419 scam",
+                        body: "You've won the prize!!!1!1!!!"
+                    }
+                ]
+            }
+        ];
+
       return (
         <div className="container">
             <div className="columns">
             <div className="col-4 col-lg-3 col-md-2 col-sm-1 col-xs-0"></div>
 
             <div className="col-4 col-lg-6  col-md-8 col-sm-10 col-xs-12">
-              {this.state.auth ? <Email/> : <Welcome/> }
+              {this.state.auth ? <Email mailboxes={fixtures}/> : <Welcome/> }
             </div>
 
             <div className="col-4 col-lg-3 col-md-2 col-sm-1 col-xs-0"></div>
@@ -24,6 +69,10 @@ class App extends React.Component {
         </div>
       )
   }
+
 }
+
+
+
 
 export default App;
--- Frontend/src/email.js
+++ Frontend/src/email.js
@@ -1,12 +1,291 @@
 import React from 'react';
+import './email.css';
 
+var InboxMails = []
+var SentMails = []
 class Email extends React.Component {
-  render(){
-      return (
-        <div className="text-center">
-          <h1>You are in!</h1>
-        </div>
+    constructor() {
+        super();
+        this.inboxFunction = this.inboxFunction.bind(this); //for listing mails that are received.
+        this.sentFunction = this.sentFunction.bind(this);   //for listing mails that are sent.
+        this.mailContent = this.mailContent.bind(this);     //for displaying the content of the selected mail
+        this.sendMail = this.sendMail.bind(this);           //forum for send a mail
+        this.handleSendSubmit = this.handleSendSubmit.bind(this);   //For handling inputs to send a mail
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            //initial mails list div 
+            mailsContent: <tr >  <td colspan="2" id="noselected_div">   
+                No Folder selected.
+            </td></tr>,
+
+            //initial mail body div 
+            mailBody: <div className="noselected_div">
+                No Mail selected.
+            </div>,
+
+            mail_list_header1: "",  //Mails list table header can be changed according to sent or received mail (To/From)
+            mail_list_header2: "",  //Holds "Subject" header
+
+            email_to_send: "",  //this states are for saving sending mail info
+            subject_to_send: "",
+            message_to_send: ""
+
+
+        };
+}
+
+ 
+    inboxFunction() {
+
+        //This function is for listing mails that are received.
+
+        const list = InboxMails.map((item) => 
+     
+            <tr onClick={() => this.mailContent(item, 0)}>
+                <td>{item.from}</td>
+                <td>{item.subject}</td>
+            </tr>
+            
+        );
         
+        this.setState({
+            mailsContent: list,
+            mail_list_header1: "From",
+            mail_list_header2: "Subject"
+        });
+
+    }
+
+    sentFunction() {
+
+        //This function is for listing mails that are sent.
+
+        const list = SentMails.map((item) =>
+
+            <tr onClick={() => this.mailContent(item, 1)}>
+                <td>{item.to}</td>
+                <td>{item.subject}</td>
+            </tr>
+
+        );
+
+        this.setState({
+            mailsContent: list,
+            mail_list_header1: "To",
+           mail_list_header2: "Subject"
+        });
+
+    }
+
+    mailContent(item, id) {
+        //This function is for displaying the content of the selected mail
+
+        var from_to = "From: "  //If a received mail is wanted to display, it changes the header of the table
+        var address = item.from
+        if (id === 1) {
+            from_to = "To: "    //If a sent mail is wanted to display
+            address = item.to
+        }
+            
+        const content =
+            <div className="mailbody_div">
+                <table>
+
+                    <tr>
+                        <td><h5>{from_to} </h5></td>
+                        <td> <h6>{address}</h6></td>
+                    </tr>
+
+                    <tr>
+                        <td><h5>Subject:  </h5></td>
+                        <td> <h6>{item.subject}</h6></td>
+                    </tr>
+                    
+                </table>
+
+                <hr size="10"/>
+                <p>  {item.content}</p>
+            </div>
+
+        this.setState({
+            mailBody: content
+        });
+
+    }
+
+    sendMail() {
+
+        //This function changes the mail content div to be able to send a mail, it gives a form: "mail to send", "subject to send" and "message to send"
+        this.setState({
+            mailBody: 
+        
+
+       <form className="form-horizontal" action="#forms" onSubmit={this.handleSendSubmit}>
+            <div className="form-group">
+            <div className="col-3 col-sm-12">
+                <label className="form-label" for="input-example-4"><h5>To: </h5></label>
+            </div>
+            <div className="col-9 col-sm-12">
+                            <input className="form-input" id="input-example-4"
+                                type="email"
+                                placeholder="Email"
+                                name="email_to_send"
+                                onChange={this.handleChange}
+                            />
+            </div>
+            </div>
+            <div className="form-group">
+            <div className="col-3 col-sm-12">
+                <label className="form-label" for="input-example-5"><h5>Subject: </h5></label>
+            </div>
+            <div className="col-9 col-sm-12">
+                            <input className="form-input" id="input-example-5"
+                                type="subject"
+                                placeholder="Subject"
+                                name="subject_to_send"
+                                onChange={this.handleChange}/>
+            </div>
+            </div>
+            
+            <div className="form-group">
+            <div className="col-3 col-sm-12">
+                <label className="form-label" for="input-example-6"><h5>Message: </h5></label>
+            </div>
+            <div className="col-9 col-sm-12">
+                            <textarea className="form-input" id="input-example-6"
+                                placeholder="Textarea"
+                                rows="3"
+                                name="message_to_send"
+                                onChange={this.handleChange}></textarea>
+            </div>
+            </div>
+
+             <div className="form-group">
+                <div className="btn-group btn-group-block">
+                    <button className="btn btn-lg" id= "sendemail_button" type="submit">Send Email</button>
+                </div>
+            </div>
+            
+        </form>
+
+        });
+
+        
+    }
+    //For handling inputs(mail to send, subject and message) from sending mail menu
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+   
+    }
+    handleSendSubmit(e) {
+        alert(this.state.email_to_send + this.state.subject_to_send + this.state.message_to_send) //for testing
+        e.preventDefault()
+    }
+    render() {
+              
+        InboxMails = []     //Holds inbox mails info
+        SentMails = []      //Holds sent mails info
+
+        //In this code all mails (sending and receiving) are taken as props from App.js, separates and saves mails info into arrays
+        for (var i = 0; i < this.props.mailboxes[0].emails.length; i++) { //Inbox
+            
+            InboxMails.push({
+                from: this.props.mailboxes[0].emails[i].from,
+                subject: this.props.mailboxes[0].emails[i].subject,
+                content: this.props.mailboxes[0].emails[i].body
+            })
+            
+        }
+        for (i = 0; i < this.props.mailboxes[1].emails.length; i++) { //Sent
+            SentMails.push({
+                to: this.props.mailboxes[1].emails[i].from,
+                subject: this.props.mailboxes[1].emails[i].subject,
+                content: this.props.mailboxes[1].emails[i].body
+            })
+        }
+
+
+        
+      return (
+        
+          //Layout: "main div=> app_div(has all subdivs)", "header div", "menu div(left side)", "mails list div" and "mail content div"
+          <div className="flex-centered">
+              
+              <div className="app_div">
+
+                  <div className="header_section">
+                      <p className="header_title">A Voice Based Email System</p>
+                  </div>
+
+                  <div className="menu_div">
+
+                          <ul class="menu">
+                              <li class="menu-item">
+                                  <div class="tile tile-centered">
+                                          <div class="tile-content">Menu</div>
+                                   </div>
+                              </li>
+                               <li class="divider"></li>
+                                <li class="menu-item" onClick={this.sendMail}><a href="#">Send Email</a>     
+                                </li>
+                                <li class="menu-item"><a href="#">Listen Email</a>
+                                    
+                                </li>   
+                          </ul>
+
+                      <div className="mailbox_div">
+                          <h4>Folders</h4>
+                          <ul className="mailboxitem_div">
+
+                              <li className="item_div" key={0}>
+
+                                  <button class="btn badge" data-badge={InboxMails.length} onClick={this.inboxFunction}>
+                                      Inbox
+                                   </button>
+
+
+                              </li>
+
+                              <li className="item_div" key={1}>
+
+                                  <button class="btn badge" data-badge={SentMails.length} onClick={this.sentFunction}>
+                                      Sent
+                                   </button>
+
+                              </li>
+                          </ul>
+
+                      </div>
+
+                   </div>
+
+                  <div className="mails_div">
+                      <table className="email-list table table-striped table-condensed">
+                          <thead>
+                              <tr>
+                                  <th width="70%">{this.state.mail_list_header1}</th>
+                                  <th width="30%">{this.state.mail_list_header2}</th>
+                                 
+                              </tr>
+                          </thead>
+                          <tbody>
+                              {this.state.mailsContent}
+                          </tbody>
+                      </table>
+                      
+                  </div>
+
+                  <div className="mailcontent_div">
+                      {this.state.mailBody}
+                  </div>
+
+              </div>
+           
+
+          </div>
       )
   }
 }
